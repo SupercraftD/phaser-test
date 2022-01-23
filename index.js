c024279c47634057b8ef948487fed2f8fc1f8273
SupercraftD <72275050+SupercraftD@users.noreply.github.com>
2022-01-23 14:20:50 -0500

    g.v.0.0.1

---
+++ index.js
@@ -0,0 +1,30 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.setBaseURL('https://supercraftd.github.io/phaser-test/');
+
+    this.load.image('sky', 'assets/sky.jpeg');
+
+}
+
+function create ()
+{
+    this.add.image(0, 0, 'sky');
+}
